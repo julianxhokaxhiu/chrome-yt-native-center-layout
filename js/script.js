feff077571e5eab879ace0bef0801587b07d11ca
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2013-01-10 12:40:26 +0100

    Update YT Native Center Layout/js/script.js
    Little fix so it can work always and everywhere.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 document.body.classList.remove('site-left-aligned');
 var tmp = setInterval(function(){
-  if(document.getElementById('watch7-video-container').style.backgroundColor == 'rgb(0, 0, 0)')document.body.classList.add('black-video-background');
-	if(document.readyState)clearInterval(tmp);
-},250);
+    if(document.getElementById('watch7-video-container').style.backgroundColor == 'rgb(0, 0, 0)')document.body.classList.add('black-video-background');
+    if(document.getElementById('watch7-player').classList.contains('player-root'))clearInterval(tmp);
+},1000);
