5d60b0f5ddd3a8fa016563c0644b0f14c695b979
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2013-01-10 10:29:01 +0100

    Update YT Native Center Layout/js/script.js
    Add a class if the video has a black background.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
-document.body.classList.remove('site-left-aligned')
+document.body.classList.remove('site-left-aligned');
+if(document.getElementById('watch7-video-container').style.backgroundColor == 'rgb(0, 0, 0)')document.body.classList.add('black-video-background')
