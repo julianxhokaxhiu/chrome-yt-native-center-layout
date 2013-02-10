94b8a59b5b8a8840bbaf9b20be5b0f134eaf29e4
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2013-02-10 19:50:37 +0100

    Updated to v0.1.11
    * Added support for purple video area background

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 var blackBgFix = setInterval(function(){
-    if(document.getElementById('watch7-video-container').style.backgroundColor == 'rgb(0, 0, 0)')document.body.classList.add('black-video-background');
+    if(document.getElementById('watch7-video-container').style.backgroundColor > ''){
+        if(document.getElementById('watch7-video-container').style.backgroundColor == 'rgb(0, 0, 0)')document.body.classList.add('black-video-background');
+        else if(document.getElementById('watch7-video-container').style.backgroundColor == 'rgb(38, 0, 30)')document.body.classList.add('purple-video-background');
+    }
     if(document.getElementById('watch7-player').classList.contains('player-root'))clearInterval(blackBgFix);
 },1000);
 var sidebarFix = setInterval(function(){
