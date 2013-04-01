9d5e943e40c366e0b42c8d47ba89ae0671da68fb
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2013-04-01 18:22:32 +0200

    Updated to v0.1.16
    * Reliagned overall styles to center again most parts of Youtube (from
    video pages to playlists)

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 **AUTHOR:** Julian Xhokaxhiu
 
-**VERSION:** 0.1.15
+**VERSION:** 0.1.16
 
 **DESCRIPTION:** Center the Youtube layout
 
--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
+var parent = null;
 var blackBgFix = setInterval(function(){
-    if(document.getElementById('watch7-video-container').style.backgroundColor > ''){
-        if(document.getElementById('watch7-video-container').style.backgroundColor == 'rgb(0, 0, 0)')document.body.classList.add('black-video-background');
-        else if(document.getElementById('watch7-video-container').style.backgroundColor == 'rgb(38, 0, 30)')document.body.classList.add('purple-video-background');
+    if(document.getElementById('player').style.backgroundColor > ''){
+        if(document.getElementById('player').style.backgroundColor == 'rgb(0, 0, 0)')document.body.classList.add('black-video-background');
+        else if(document.getElementById('player').style.backgroundColor == 'rgb(38, 0, 30)')document.body.classList.add('purple-video-background');
     }
-    if(document.getElementById('watch7-player').classList.contains('player-root'))clearInterval(blackBgFix);
+    clearInterval(blackBgFix);
 },1000);
 var sidebarFix = setInterval(function(){
     if(document.body.classList.contains('sidebar-collapsed')){
